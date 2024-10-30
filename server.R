589b0570feeb5c4e2811736c784843a8087da90b
Krzysztof Warzecha <flashlight.studio@pm.me>
2024-10-30 16:04:11 +0100

    added code for file input preview table for data input tab

--- server.R
+++ server.R
@@ -3,8 +3,20 @@ library(DT)
 
 server <- function(input, output, session) {
   
+  # table for Config tab
   output$categories_table <- renderDT({
     file <- input$categories_import
+    ext <- tools::file_ext(file$datapath)
+    
+    req(file)
+    validate(need(ext == "csv", "Please upload a csv file"))
+    
+    read.csv(file$datapath, header = TRUE)
+  }, server = FALSE, rownames = FALSE, editable = FALSE, selection = 'none')
+  
+  # table for Data input tab
+  output$data_finance_table <- renderDT({
+    file <- input$data_finace_import
     ext <- tools::file_ext(file$datapath)
     
     req(file)
