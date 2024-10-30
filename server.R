b19b4a9d3cabdffbbbc87631f450b8dacfae3168
Krzysztof Warzecha <flashlight.studio@pm.me>
2024-10-30 20:41:47 +0100

    changed importing data from file inputs, objects moved outside renderDT function to reactive values

--- server.R
+++ server.R
@@ -3,25 +3,44 @@ library(DT)
 
 server <- function(input, output, session) {
   
-  # table for Config tab
-  output$categories_table <- renderDT({
+  # config data
+  categories_tbl <- reactiveVal()
+  
+  # populate reactive value with input table
+  observeEvent(input$categories_import, {
     file <- input$categories_import
     ext <- tools::file_ext(file$datapath)
     
     req(file)
     validate(need(ext == "csv", "Please upload a csv file"))
     
-    read.csv(file$datapath, header = TRUE)
+    input_tbl <- read.csv(file$datapath, header = TRUE)
+    categories_tbl(input_tbl)
+  })
+  
+  # table for Config tab
+  output$categories_table <- renderDT({
+    categories_tbl()
   }, server = FALSE, rownames = FALSE, editable = FALSE, selection = 'none')
   
-  # table for Data input tab
-  output$data_finance_table <- renderDT({
+  # financial data
+  fin_data <- reactiveValues()
+  
+  # populate reactive value with input table
+  observeEvent(input$data_finace_import, {
     file <- input$data_finace_import
     ext <- tools::file_ext(file$datapath)
     
     req(file)
     validate(need(ext == "csv", "Please upload a csv file"))
     
-    read.csv(file$datapath, header = TRUE)
+    input_tbl <- read.csv(file$datapath, header = TRUE)
+    fin_data$input_tbl
+  })
+  
+  # table for Data input tab
+  output$categories_table <- renderDT({
+    fin_data$input_tbl
   }, server = FALSE, rownames = FALSE, editable = FALSE, selection = 'none')
+  
 }
